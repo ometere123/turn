dd3cd2fa787443fb99f18bb310ccb331cc43723e
papitodele <45469370+ometere123@users.noreply.github.com>
2026-09-12 03:50:45 +0100

    ux: install slow-confirmation guidance

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
 import { TurnTools } from './components/TurnTools.tsx'
+import { installLatencyCopy } from './lib/latency-ux.ts'
 import './styles.css'
+
+installLatencyCopy()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
