a5f1a21b0f0cc64d4d38d6d8fa9a26d5afcbd8bb
papitodele <45469370+ometere123@users.noreply.github.com>
2026-09-12 03:49:15 +0100

    feat: mount turn tools

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App.tsx'
+import { TurnTools } from './components/TurnTools.tsx'
 import './styles.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <TurnTools />
   </StrictMode>,
 )
